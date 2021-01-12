917195493c7341fc1b97b5397d6ed4de073fc275
Hietzu22 <veeti.hietanen03@gmail.com>
2021-01-12 09:33:35 +0200

    added initial changes/first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include_once 'layout/top.inc.php'; ?>
+<?php include_once 'layout/nav.inc.php'; ?>
+
+  <div class="jumbotron">
+      <h1 class="display-3">Welcome To The Voting App</h1>
+    </div>
+
+<?php include_once 'layout/bot.inc.php'; ?>
